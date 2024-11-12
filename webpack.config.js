cd09a1625c4a7732a985175d1baef5d34686281c
Keno Leon <me@k3no.com>
2024-11-11 20:04:58 -0600

    initial Commits

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true, // Ensure the dist directory is cleaned before each build
   },
   module: {
     rules: [
@@ -13,5 +15,15 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
   },
 };
